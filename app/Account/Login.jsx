c723aa5b77c543af222ab7dd5baa2ba125b3d659
Ganeshdwivedi17 <139634932+Ganeshdwivedi17@users.noreply.github.com>
2023-12-02 18:22:08 +0530

    Update Login.jsx

--- app/Account/Login.jsx
+++ app/Account/Login.jsx
@@ -45,26 +45,23 @@ function Login() {
       >
       {errmsg}
       </p>
-        <div className='md:w-[99.5vw] sm:w-[100vw] bg-cover bg-center bg-[url("https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHNob2VzfGVufDB8fDB8fHww")] z-0 lg:mt-20  font-[FFDINforPumaW07-Regular-latin] relative overflow-x-clip '>
-        <video className='w-[100vw] h-[150%] Register-video sm:hidden md:block' autoPlay muted loop >
-            <source  src='https://static.videezy.com/system/resources/previews/000/053/100/original/4K-44.mp4' type='video/mp4' ></source>
-        </video>
-        <div className="m-36 sm:m-2 md:absolute md:top-[5%] md:left-[25%]">
-        <h1 className="text-4xl font-extrabold text-left lg:pl-20 pb-5 ">
+        <div className='md:w-[99.5vw] sm:mt-16 sm:w-[100vw] bg-cover bg-center bg-[url("https://img.freepik.com/free-vector/gradient-glassmorphism-horizontal-banner_23-2149440108.jpg")] lg:mt-20  font-[FFDINforPumaW07-Regular-latin] relative overflow-x-clip '>
+        <div className="m-36 sm:m-2 ">
+        <h1 className="text-4xl sm:pt-2 font-extrabold text-left lg:pl-20 pb-5 ">
           My Account
         </h1>
-        <div className="m-14 flex justify-center">
+        <div className=" flex justify-center">
           <form
-            className="md:w-[37vw] sm:w-full"
+            className="md:w-[37vw] rounded-md p-10 bg-white bg-opacity-30 backdrop-blur-[6px] sm:w-full"
             onSubmit={handleSubmit}
           >
             <div className="flex justify-center flex-col items-center">
-              <p className="font-bold">Login / Register</p>
+              <p className="font-bold ">Login / Register</p>
               <hr className="h-[2px] w-full mt-4 mb-8 bg-gray-200 border-0 dark:bg-gray-700" />
             </div>
             <div className="flex flex-col justify-start items-center">
               <p className="text-slate-700 text-sm self-start">
-                Enter your email and password to log in
+                Enter your username and password to log in
               </p>
               <div className="flex flex-col my-3">
                 <label className="text-sm font-bold">Username</label>
@@ -75,7 +72,7 @@ function Login() {
                     setUser(e.target.value);
                   }}
                   required
-                  className="md:w-[37vw] sm:w-full px-4 py-3 border border-solid border-black"
+                  className="md:w-[35vw] sm:w-full px-4 py-3 border border-solid border-black"
                   type="text"
                   placeholder="Enter your Username"
                 />
@@ -89,7 +86,7 @@ function Login() {
                   }}
                   required
                   autoComplete='true'
-                  className="md:w-[37vw] sm:w-full px-4 py-3 border border-solid border-black"
+                  className="md:w-[35vw] sm:w-full px-4 py-3 border border-solid border-black"
                   type="password"
                   placeholder="Enter your password"
                 />
